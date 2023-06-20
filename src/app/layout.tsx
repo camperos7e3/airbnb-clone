c587f817687905122b6cffe4ac3ff2289c6384d4
camperos7e3 <camperos.7e3@proton.me>
2023-06-20 17:45:21 +0200

    implement ClientOnly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import RentModal from '@components/models/RentModal'
 
 import ToasterProvider from '@providers/ToasterProvider'
 import getCurrentUser from '@actions/getCurrentUser'
+import ClientOnly from '@components/ClientOnly'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -26,11 +27,13 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <ToasterProvider />
-        <RentModal />
-        <LoginModal />
-        <RegisterModal />
-        <Navbar currentUser={currentUser} />
+        <ClientOnly>
+          <ToasterProvider />
+          <RentModal />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
         <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
