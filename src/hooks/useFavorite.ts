6d1de4244b0cf3dff63e0c05e9a79ca3a577dcb6
camperos7e3 <camperos.7e3@proton.me>
2023-06-20 17:08:18 +0200

    create user favorites hook

---
+++ src/hooks/useFavorite.ts
@@ -0,0 +1,63 @@
+import { useRouter } from 'next/navigation'
+import { useCallback, useMemo } from 'react'
+import { toast } from 'react-hot-toast'
+
+import { type SafeUser } from '@types'
+
+import useLoginModal from './useLoginModal'
+
+interface IUseFavorite {
+  listingId: string
+  currentUser?: SafeUser | null
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter()
+
+  const loginModal = useLoginModal()
+
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favoriteIds ?? []
+
+    return list.includes(listingId)
+  }, [currentUser, listingId])
+
+  const toggleFavorite = useCallback(
+    async (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation()
+
+      if (!currentUser) {
+        loginModal.onOpen()
+        return
+      }
+
+      try {
+        let request
+
+        if (hasFavorited) {
+          request = () =>
+            fetch(`/api/favorites/${listingId}`, { method: 'delete' })
+        } else {
+          request = () =>
+            fetch(`/api/favorites/${listingId}`, { method: 'post' })
+        }
+
+        console.log(request)
+
+        await request()
+        router.refresh()
+        toast.success('Success')
+      } catch (error) {
+        toast.error('Something went wrong.')
+      }
+    },
+    [currentUser, hasFavorited, listingId, loginModal, router]
+  )
+
+  return {
+    hasFavorited,
+    toggleFavorite
+  }
+}
+
+export default useFavorite
