01fe854bb0a6ccde95aace1e9273087900eb9d5f
camperos7e3 <camperos.7e3@proton.me>
2023-06-20 17:18:12 +0200

    Create add listing api

---
+++ src/app/api/listings/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server'
+
+import prisma from '@libs/prismadb'
+import getCurrentUser from '@/app/actions/getCurrentUser'
+
+// todo: CREATE EXTERNAL API
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser()
+
+  if (!currentUser) return NextResponse.error()
+
+  const body = await request.json()
+  const {
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price
+  } = body
+
+  Object.keys(body).forEach((value: any) => {
+    if (!body[value]) return NextResponse.error()
+  })
+
+  console.log(location)
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location != null ? location.value : '',
+      price: parseInt(price, 10),
+      userId: currentUser.id
+    }
+  })
+
+  return NextResponse.json(listing)
+}
