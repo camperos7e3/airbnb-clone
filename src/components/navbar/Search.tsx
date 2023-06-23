3c52382ee136e490edb2cd2a20d4b41d0bd8f88a
camperos7e3 <camperos.7e3@proton.me>
2023-06-23 17:43:25 +0200

    implement searchModal and searchParams

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -1,10 +1,54 @@
 'use client'
 
+import { useSearchParams } from 'next/navigation'
+import { useMemo } from 'react'
 import { BiSearch } from 'react-icons/bi'
+import { differenceInDays } from 'date-fns'
+
+import useSearchModal from '@hooks/useSearchModal'
+import useCountries from '@hooks/useCountries'
 
 const Search = () => {
+  const searchModal = useSearchModal()
+  const params = useSearchParams()
+  const { getByValue } = useCountries()
+
+  const locationValue = params?.get('locationValue')
+  const startDate = params?.get('startDate')
+  const endDate = params?.get('endDate')
+  const guestCount = params?.get('guestCount')
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue)?.label
+    }
+
+    return 'Anywhere'
+  }, [locationValue, getByValue])
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate)
+      const end = new Date(endDate)
+      let diff = differenceInDays(end, start)
+
+      if (diff === 0) diff = 1
+
+      return `${diff} Days`
+    }
+
+    return 'Any Week'
+  }, [startDate, endDate])
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) return `${guestCount} Guests`
+
+    return 'Add Guests'
+  }, [guestCount])
+
   return (
     <div
+      onClick={searchModal.onOpen}
       className="
         border-[1px]
         w-full
@@ -23,7 +67,7 @@ const Search = () => {
           flex-row
           items-center
           justify-between
-          "
+        "
       >
         <div
           className="
@@ -32,7 +76,7 @@ const Search = () => {
             px-6
           "
         >
-          Anywhere
+          {locationLabel}
         </div>
         <div
           className="
@@ -46,7 +90,7 @@ const Search = () => {
             text-center
           "
         >
-          Any Week
+          {durationLabel}
         </div>
         <div
           className="
@@ -60,7 +104,7 @@ const Search = () => {
             gap-3
           "
         >
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div
             className="
               p-2
@@ -69,7 +113,7 @@ const Search = () => {
               text-white
             "
           >
-            <BiSearch />
+            <BiSearch size={18} />
           </div>
         </div>
       </div>
